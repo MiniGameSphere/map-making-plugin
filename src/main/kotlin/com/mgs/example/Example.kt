e21bf8ab3c49e73ac15c0491293c6a37a99d0a1e
FubyCutie <roset280905@gmail.com>
2023-08-18 16:34:19 +0100

    initial commit

---
+++ src/main/kotlin/com/mgs/example/Example.kt
@@ -0,0 +1,15 @@
+package com.mgs.example;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+class Example : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+
+    }
+
+    override fun  onDisable() {
+        // Plugin shutdown logic
+    }
+}
