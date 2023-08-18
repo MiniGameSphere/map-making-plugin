66344dd276d444ea5badcc3eae9401b77f7104d1
FubyCutie <roset280905@gmail.com>
2023-08-18 17:15:37 +0100

    added worldedit

--- src/main/kotlin/com/mgs/map_making/Main.kt
+++ src/main/kotlin/com/mgs/map_making/Main.kt
@@ -5,11 +5,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 class Main : JavaPlugin() {
 
     override fun onEnable() {
-        // Plugin startup logic
-
     }
 
     override fun  onDisable() {
-        // Plugin shutdown logic
     }
 }
